884e801a27a7885eacc87f9226b42dfc5c6d02a8
nnnnnzo <enzo.lagadec@etu.univ-savoie.fr>
2023-03-10 16:55:03 +0100

    2

--- odoo/local-src/students/models/students.py
+++ odoo/local-src/students/models/students.py
@@ -13,10 +13,23 @@ class StudentsTraining(models.Model):
         inverse_name="training_id",
     )
 
+class StudentsMark(models.Model):
+    _name = "students.mark"
+    _description = "Mark table"
+    subject = fields.Char(string="Mark subject", size=4, required=True)
+    mark = fields.Float(string="Mark", size=100, required=True)
+    student_ids = fields.Many2one(
+        string="Student",
+        comodel_name="students.student",
+        ondelete="cascade",
+        required=True,
+    )
+
 
 class StudentsStudent(models.Model):
     _name = "students.student"
     _description = "Student table"
+    _rec_name = "number"
     number = fields.Char("Student number", size=11, required=True)
     firstname = fields.Char("Student firstname", size=64, required=True)
     lastname = fields.Char("Student lastname", size=64, required=True)
@@ -24,4 +37,9 @@ class StudentsStudent(models.Model):
         string="Training",
         comodel_name="students.training",
         ondelete="cascade",
+    )
+    mark_ids = fields.One2many(
+        string="Student Marks",
+        comodel_name="students.mark",
+        inverse_name="student_ids",
     )
